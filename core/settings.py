594e6dfd300518076e6f34f926626672b434e125
Breider093 <Breider093@gmail.com>
2026-09-22 23:44:55 -0500

    Update CORS origin for local IP address

--- core/settings.py
+++ core/settings.py
@@ -87,7 +87,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5500',
     'http://localhost:5500',
-    '192.168.1.4',
+    'http://192.168.1.4',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
